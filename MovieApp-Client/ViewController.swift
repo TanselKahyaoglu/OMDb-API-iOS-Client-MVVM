dcacdd137cd2aeaac4c3ea6a2ebd9c1b176dee5d
Tansel Kahyaoglu <tansel.kahyaoglu@appcent.mobi>
2020-08-29 14:03:54 +0300

    Initial commit

---
+++ MovieApp-Client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp-Client
+//
+//  Created by Tansel Kahyaoglu on 29.08.2020.
+//  Copyright © 2020 Tansel Kahyaoglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
